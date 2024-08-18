e32abdf6025e59f455046e6752e6baa57598f31a
Patrick Murimi <murimipatrick101@gmail.com>
2024-08-18 16:43:29 +0300

    added new files

---
+++ django-models/relationship_app/query_samples.py
@@ -0,0 +1,22 @@
+from .models import Book, Author, Library, Librarian 
+
+#query all books by specific authors
+
+def get_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
+    return books
+
+#quert list all books in a library
+
+def get_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    Books = Book.objects.all()
+    return Books
+
+#Retrieve the Libraian for a librry
+
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    Librarian = Librarian.objects.get(library=library)
+    return Librarian
