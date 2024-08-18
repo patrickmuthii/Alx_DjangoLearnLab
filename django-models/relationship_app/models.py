b37d42d879ef1ebca6af86c7a20606da83235aa6
Patrick Murimi <murimipatrick101@gmail.com>
2024-08-18 16:50:57 +0300

    new changes

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -11,7 +11,7 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    Author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.title
