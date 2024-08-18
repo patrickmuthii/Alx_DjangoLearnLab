ee2a48dca2fd2870494b10806bd681f7aad530e6
Patrick Murimi <murimipatrick101@gmail.com>
2024-08-18 17:38:40 +0300

    added new file

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 from .models import Book, Author, Library, Librarian
-from django.views.generic import DetailView
+#from django.views.generic import DetailView
+from django.http import HttpResponse
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    response_text = '\n'.join([f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(response_text, content_type='text/plain'
 
 
 
