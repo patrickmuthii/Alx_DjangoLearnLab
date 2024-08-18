46f4160f0ca5f83312314ec3e8e2a82ef272bac0
Patrick Murimi <murimipatrick101@gmail.com>
2024-08-18 17:25:55 +0300

    new changes

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Book, Author, Library, Librarian
+from django.http import HttpResponse
+from django.template import loader
+from django.views.generic import ListView
 
-# Create your views here.
+#list all books in the database
+def list_books(request):
+    return render(request, 'list_books.html')
+
+class ListView(request):
+    models = Book
+    template_name = 'list_books.html'
+    return render(request (template_name = 'list_books.html')books = Book.objects.all())
+
