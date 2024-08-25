80fdf6b8e6bfb67daaaac5cc5131d4a9d675d991
Patrick Murimi <murimipatrick101@gmail.com>
2024-08-25 15:24:58 +0300

    added templates folder

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -12,7 +12,7 @@ class CustomUserChangeForm(UserChangeForm):
         model = CustomUser
         fields = ('username', 'date_of_birth', 'profile_photo')
 
-class BookForm(forms.ModelForm):
+class ExampleForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author']
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
 from django.contrib.auth.decorators import login_required, permission_required
-from .forms import BookForm
+from .forms import ExampleForm
 # Create your views here.
 
 
@@ -25,12 +25,12 @@ def add_book(request):
 def edit_book(request, pk):
     book = get_object_or_404(Book,pk=pk)
     if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)
+        form = ExampleForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
             return redirect('view_books')
     else:
-        form = BookForm(instance=book)    
+        form = ExampleForm(instance=book)    
     return render(request, 'bookshelf/edit_book.html', {'book': book})
 
 @permission_required('bookshelf.can_delete_book',raise_exception=True)
