d474cf85429d537d5f63cc07294342987ce7835a
Patrick Murimi <murimipatrick101@gmail.com>
2024-08-29 00:44:55 +0300

    Normalize all line endings

--- api_project/api_project/api_project/settings.py
+++ api_project/api_project/api_project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'api.apps.ApiConfig',
+    'api',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
