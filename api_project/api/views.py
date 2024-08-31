8bb74fd2b9e27efa81a7d9e32ee67e77babafde9
Patrick Murimi <murimipatrick101@gmail.com>
2024-08-31 23:09:36 +0300

    new changes

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,7 +5,7 @@ from .serializers import BookSerializer
 from rest_framework import viewsets
 # Create your views here.
 
-class BookViewset(viewsets.ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
