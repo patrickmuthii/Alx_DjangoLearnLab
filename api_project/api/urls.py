50c2c119156f224da9520e58091d1414a6983351
Patrick Murimi <murimipatrick101@gmail.com>
2024-08-31 23:40:48 +0300

    mnew insertions

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -9,4 +9,5 @@ router.register(r'books', BookList)
 urlpatterns = [
     path('books/', BookList.as_view(), name='book_list'),
     path('', include(router.urls))
+    path('api/token/' obtain_auth_token, name='api-token'),
 ]
