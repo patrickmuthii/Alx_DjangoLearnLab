977570a9d21d45dfecb2577333fbd2516d027c70
Patrick Murimi <murimipatrick101@gmail.com>
2024-09-06 01:35:28 +0300

    modified view

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,10 +3,10 @@ from .views import BooklistView, BookDetailView, BookCreateView, BookUpdateView,
 
 
 urlpatterns = [
-    path('book', BooklistView.as_view(), name='book_list'),
-    path('book/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
-    path('book/create/', BookCreateView.as_view(), name='book_create'),
-    path('book/<int:pk>/update/', BookUpdateView.as_view(), name='book_update'),
-    path('book/<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),
+    path('books', BooklistView.as_view(), name='book_list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('books/create/', BookCreateView.as_view(), name='book_create'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book_update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),
 
 ]
