d122a9b5a9ae7eeeb6785f83479794621df3d359
Patrick Murimi <murimipatrick101@gmail.com>
2024-09-11 02:27:01 +0300

    new

--- django_blog/blog/static/blog/js/scripts.js
+++ django_blog/blog/static/blog/js/scripts.js
@@ -1,3 +1,4 @@
+
 // Basic example script to demonstrate dynamic behavior
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Blog page loaded');
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -116,11 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/blog/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "blog/static",
 ]
-STATIC_ROOT = "static"
+STATIC_ROOT = "staticfiles/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
