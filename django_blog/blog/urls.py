e3b95bfc600921da22fa71ea9ca76cd3edb9bff1
Patrick Murimi <murimipatrick101@gmail.com>
2024-09-12 00:46:51 +0300

    modified

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('posts/', views.posts_view, name='posts'),  # Ensure 'posts' view exists
     
     path('register/', views.register_view, name='register'),
