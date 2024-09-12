89621268687ee0a9ec55b191bb02ba103ce2b786
Patrick Murimi <murimipatrick101@gmail.com>
2024-09-13 01:28:48 +0300

    added comment

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),    
-
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_edit'),
 ]    
