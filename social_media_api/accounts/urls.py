94b977d4b6edaf24000ff5d3c22a033bd4d26986
Patrick Murimi <murimipatrick101@gmail.com>
2024-09-22 01:59:24 +0300

    one more

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('user/', UserListView.as_view(), name='user'),
-    path('follow/<str:username>/', FollowUserView.as_view(), name='follow'),
-    path('unfollow/<str:username>/', UnfollowUserView.as_view(), name='unfollow'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow'),
 ]
