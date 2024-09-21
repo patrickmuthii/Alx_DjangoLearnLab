8d5d1f6e7cee3459c0c87e7a3792f4631e3a74f9
Patrick Murimi <murimipatrick101@gmail.com>
2024-09-21 11:13:22 +0300

    new

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -13,13 +13,14 @@ from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
 # Get the custom user model
-User = get_user_model()
+
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
     password_confirm = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
     class Meta:
+        User = get_user_model()
         model = User  # Use the custom user model
         fields = ['username', 'email', 'password', 'password_confirm', 'bio', 'profile_picture']
 
@@ -33,9 +34,10 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         # Remove the password_confirm field as it's not needed for user creation
         validated_data.pop('password_confirm')
         # Use create_user from the custom user model to create the user
+        User = get_user_model()
         user = User.objects.create_user(**validated_data)
         # Create a token for the new user
-        Token.objects.create(user=user)
+        Token.objects.create_user(user)
         return user
 
 
