581bbfdd2b2ca4fb2602c291733dfaa5eec96c06
Patrick Murimi <murimipatrick101@gmail.com>
2024-09-21 12:00:27 +0300

    new

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,46 +1,29 @@
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
-
-
-
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
-
-# Get the custom user model
-from django.contrib.auth import get_user_model
-from rest_framework import serializers
-from rest_framework.authtoken.models import Token
-
-# Get the custom user model
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
-    password_confirm = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
-
     class Meta:
-        User = get_user_model()
-        model = User  # Use the custom user model
-        fields = ['username', 'email', 'password', 'password_confirm', 'bio', 'profile_picture']
-
-    def validate(self, data):
-        # Ensure passwords match
-        if data['password'] != data['password_confirm']:
-            raise serializers.ValidationError("Passwords must match.")
-        return data
+        model = get_user_model()
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        # Remove the password_confirm field as it's not needed for user creation
-        validated_data.pop('password_confirm')
-        # Use create_user from the custom user model to create the user
-        User = get_user_model()
-        user = User.objects.create_user(**validated_data)
-        # Create a token for the new user
-        Token.objects.create_user(user)
+        user = get_user_model().objects.create_user(**validated_data)
+        token = Token.objects.create(user=user)
         return user
+    
+    def validate(self, attrs):
+        return super().validate(attrs)
+    
 
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data.get('password'))
+        instance.save()
+        return instance
 
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField()
-    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    password = serializers.CharField(write_only=True)   
+        
