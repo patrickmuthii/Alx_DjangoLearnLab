bfafb2e277181b71a41a2dc236e82ce267ecf952
Patrick Murimi <murimipatrick101@gmail.com>
2024-09-22 02:42:49 +0300

    one more

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -4,11 +4,11 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
-router.register('posts', PostViewSet, basename='posts')
-router.register('comments', CommentViewSet, basename='comments')
+router.register('posts/', PostViewSet, basename='posts')
+router.register('comments/', CommentViewSet, basename='comments')
 router.register('feed/', FeedView, basename='feed')
-router.register('posts/<int:pk>/like', LikePostView, basename='like')
-router.register('posts/<int:pk>/unlike', UnlikePostView, basename='unlike')
+router.register("posts/<int:pk>/like/", LikePostView, basename='like')
+router.register("posts/<int:pk>/unlike/", UnlikePostView, basename='unlike')
 urlpatterns = [
     path('', include(router.urls)),
    
