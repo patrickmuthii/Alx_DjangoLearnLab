99ce51ea06cca1d9c7eb9061fe323e5f80cf7d3d
Patrick Murimi <murimipatrick101@gmail.com>
2024-09-22 21:00:50 +0300

    commit

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a_b*lu-s&$25v@7--(xo^n*5nd2vcnlg%(0exrdk_r&-s=u5id'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -81,8 +84,12 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'patrick',
+        'PASSWORD': 'patrick',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -139,3 +146,11 @@ REST_FRAMEWORK = {
           # Add any other filter backends you want here     
           ],
 }
+
+
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_C0NTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True
+
+DEBUG = False
